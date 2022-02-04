1bc8f23775348eb7c43d3c77ee7b8696db5140bc
Andrew Ta <andrewta365@gmail.com>
2022-02-03 21:14:35 -0500

    add command

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,6 @@ app.use(route);
 mongoose
     .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(result => {
-        console.log(result)
         app.listen(3000); // launches server on port 5000
     })
     .catch(err => {
