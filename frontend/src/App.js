c71e9d6a5f12eccf8fc4d619a99d680e7fee69d1
Andrew Ta <andrewta365@gmail.com>
2022-02-08 11:26:24 -0500

    add navigation and create account page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from "react-router-dom";
+import NavigationBar from "./components/navigationBar";
+import CreateAccountPage from "./pages/createAccount";
+import LoginPage from "./pages/login";
+import HomePage from "./pages/homePage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+            <Router>
+                <div>
+                    <NavigationBar />
+
+                    <Routes>
+                        <Route path='/signup' element={<CreateAccountPage />} />
+                        <Route path='/login' element={<LoginPage />} />
+                        <Route path='/' element={<HomePage />} />
+                    </Routes>
+                </div>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
