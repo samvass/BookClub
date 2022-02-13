b049a2cc99ffa87ec9d603e8e98174e06889df9b
Andrew Ta <andrewta365@gmail.com>
2022-02-12 20:18:33 -0500

    finish story test for create account

--- backend/tests/features/step_definitions/create_account_steps.js
+++ backend/tests/features/step_definitions/create_account_steps.js
@@ -5,44 +5,56 @@ const { Given, When, Then, AfterAll } = require('@cucumber/cucumber');
 
 const app = require("../../../app");
 
-let req;
+let data = {
+    username: "",
+    email: "",
+    password: "",
+}
 
 Given('a user tries to create an account', () => {
     console.log("a user tries to create an account")
 });
 
 When('a user enters a username {string}, email {string}, and password {string}', (string, string2, string3) => {
-    // create an account
-    req = request(app)
-        .post('/users/create')
-        .set('Accept', 'application/json')
-        .send({ username: string, email: string2, password: string3 })
+    data.username = string;
+    data.email = string2;
+    data.password = string3;
 });
 
 Then('a new account will be created with username {string}, email {string}, and password {string}', async (string, string2, string3) => {
-    // wait for creating account to finish
-    await req;
+    // create an account
+    await request(app)
+                .post('/users/create')
+                .set('Accept', 'application/json')
+                .send(data);
 
     // get the account
-    request(app)
-        .get('/users/get/' + string)
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .then(response => {
-            let user = response.body.user[0]
-            assert(user.username, string);
-            assert(user.email, string2);
-        })
+    await request(app)
+            .get('/users/get/' + string)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then(response => {
+                let user = response.body.user[0]
+                assert(user.username, string);
+                assert(user.email, string2);
+            })
 });
 
-Then('an error message {string} is issued', (string) => {
-    // TODO
+Then('an error message {string} is issued', async (string) => {
+    // create an account
+    let res = await request(app)
+                .post('/users/create')
+                .set('Accept', 'application/json')
+                .send(data);
+
+    assert(res.body.error.length > 0);
+    assert(res.body.error.includes(string))
 })
 
 // drop collection
 AfterAll(function (done) {
-    mongoose.connection.db.dropCollection("users", function(
+    mongoose.connection.db.dropCollection("users", function (
         err,
         result
     ) {
