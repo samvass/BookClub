885d1b8d79b0ff8bdd41b92aa076aa15f9b01bf3
TianyuZhao6 <70704675+TianyuZhao6@users.noreply.github.com>
2022-02-19 18:21:05 -0500

    Update api for account

--- frontend/src/api/userAPI.js
+++ frontend/src/api/userAPI.js
@@ -15,6 +15,23 @@ const apiGet = async (endpoint) => {
     }
 }
 
+const apiPost = async (endpoint, body) => {
+    try {
+        let res = await fetch(endpoint, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        });
+
+        return await res.json();
+    } catch (error) {
+        console.log("hell0");
+    }
+}
+
 export const getUserByUserName = async (username) => {
     let endpoint = apiURL + "/users/get/" + username;
     return await apiGet(endpoint);
@@ -22,5 +39,25 @@ export const getUserByUserName = async (username) => {
 
 export const getAllUsers = async () => {
     let endpoint = apiURL + "/users/get";
+    return await apiGet(endpoint);
+}
+
+export const createAccount = async (body) => {
+    let endpoint = apiURL + "/users/create";
+    return await apiPost(endpoint, body);
+}
+
+export const login = async (body) => {
+    let endpoint = apiURL + "/users/login";
+    return await apiPost(endpoint, body);
+}
+
+export const logout = async (body) => {
+    let endpoint = apiURL + "/users/logout";
+    return await apiPost(endpoint, body);
+}
+
+export const viewAccountByUserName = async (username) => {
+    let endpoint = apiURL + "/users/view/" + username;
     return await apiGet(endpoint);
 }
