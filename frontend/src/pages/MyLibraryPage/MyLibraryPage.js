92e9c401f974bd71eac9d66c99b1c0761465ad0d
Andrew Ta <andrewta365@gmail.com>
2022-03-26 19:41:47 -0400

    add book filter by genre

--- frontend/src/pages/MyLibraryPage/MyLibraryPage.js
+++ frontend/src/pages/MyLibraryPage/MyLibraryPage.js
@@ -5,16 +5,21 @@ import Aos from 'aos'
 import 'aos/dist/aos.css'
 
 import { getBookByName } from "../../api/bookAPI"
-import { getMyLibraryByUsername, setMyLibraryByUsername } from "../../api/userAPI"
+import { getMyLibraryByUsername, setMyLibraryByUsername, getPreferencesByUsername } from "../../api/userAPI"
 
 import { Navigate } from "react-router-dom"
 import UserContext from "../../user/UserContext"
+import SessionContext from "../../session/SessionContext"
 
 
 const MyLibraryPage = props => {
     const { username } = useContext(UserContext);
+    const { session } = useContext(SessionContext);
 
     const [userBooks, setUserBooks] = useState([])
+    const [userGenres, setUserGenres] = useState([])
+    const [selectedBooks, setSelectedBooks] = useState([])
+    const [selectedGenre, setSelectedGenre] = useState("all")
     const [redirect, setRedirect] = useState(false)
 
     useEffect(async () => {
@@ -22,17 +27,30 @@ const MyLibraryPage = props => {
             setRedirect(true)
         }
 
-        const usersBooks = await getMyLibraryByUsername(username)
-        console.log(usersBooks)
-        setUserBooks(usersBooks.myLibrary)
+        // get user preferences
+        const res = await getPreferencesByUsername(username, session);
+        const userGenres = res.data;
+        userGenres.unshift("all");
+        setUserGenres(userGenres);
+
+        const books = await getMyLibraryByUsername(username)
+        setUserBooks(books.myLibrary)
+        setSelectedBooks(books.myLibrary)
     }, [])
 
     useEffect(() => {
         Aos.init({ duration: 1000 })
     });
 
+    const updateFilter = (event) => {
+        const newSelectedGenre = event.target.value;
+        setSelectedGenre(newSelectedGenre);
+        const filteredBooks = userBooks.filter(book => newSelectedGenre=="all" ? true : book.genre[0].toLowerCase().replace(/\s/g, '').includes(newSelectedGenre.toLowerCase().replace(/\s/g, '')))
+        setSelectedBooks(filteredBooks);
+    }
 
-    const displayUserBooks = (i) => userBooks.map((book, index) => {
+
+    const displayUserBooks = (i) => selectedBooks.map((book, index) => {
 
         if (index >= i && index < i + 5) {
             return (<div key={index}>
@@ -48,6 +66,7 @@ const MyLibraryPage = props => {
                         const returnedMyLibrary = await getMyLibraryByUsername(username)
                         console.log(returnedMyLibrary.myLibrary)
                         setUserBooks(returnedMyLibrary.myLibrary)
+                        setSelectedBooks(returnedMyLibrary.myLibrary.filter(book => selectedGenre=="all" ? true : book.genre[0].toLowerCase().replace(/\s/g, '').includes(selectedGenre.toLowerCase().replace(/\s/g, ''))))
                     }, 100)
 
                 }} src={book.thumbnail} alt={book.title} key={index}></img>
@@ -55,15 +74,20 @@ const MyLibraryPage = props => {
         }
     })
 
-
-
-
     return (<div className="myLibrary">
         <h1 className="myLibraryTitle">My Library</h1>
-        {userBooks.map((row, index) => {
+
+        {/* TODO: style this */}
+        <div>
+            <select className="dropDown" value={selectedGenre} onChange={updateFilter}>
+                {userGenres.map(genre => <option key={genre} value={genre}>{genre}</option>)}
+            </select>
+        </div>
+
+        {selectedBooks.map((row, index) => {
             // create a new bookshelf every row
             if (index % 5 == 0) {
-                return (<div data-aos='slide-up'>
+                return (<div data-aos='slide-up' key={index}>
                     <div className="displayed-books">{displayUserBooks(index)}</div>
                     <img src={woodshelf} className="shelf" alt="Wood Shelf"></img>
                 </div>)
