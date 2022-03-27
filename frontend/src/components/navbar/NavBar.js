6b8f8fc973c34907fff1f7eea08504f400036aa5
TianyuZhao6 <70704675+TianyuZhao6@users.noreply.github.com>
2022-03-27 12:40:35 -0400

    Update Narbar

--- frontend/src/components/navbar/NavBar.js
+++ frontend/src/components/navbar/NavBar.js
@@ -3,7 +3,28 @@ import "./NavBar.css"
 
 import SpeedDialDown from "../SpeedDialDown/SpeedDialDown"
 
+let toggleBtn = document.getElementById("toggle-btn");
+let menuItems = document.querySelectorAll(".menu a");
+let menuActive = false;
+
 const NavBar = () => {
+
+    toggleBtn.addEventListener("click", () => {
+        if (!menuActive) {
+          menuItems[0].style.transform = "translate(150px,0)";
+          menuItems[1].style.transform = "translate(150px,90px)";
+          //menuItems[2].style.transform = "translate(90px,150px)";
+          //menuItems[3].style.transform = "translate(0,150px)";
+          menuActive = true;
+          toggleBtn.classList.add("active");
+        } else {
+          menuItems.forEach((menuItem) => {
+            menuItem.style.transform = "translate(0,0)";
+          });
+          menuActive = false;
+          toggleBtn.classList.remove("active");
+        }
+      })
 
     return (
         <div className="header">
