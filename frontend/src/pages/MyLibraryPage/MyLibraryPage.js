00e0f4b17ed1c8e694d11dc7d355419792a065b0
Adam Geenen <adamgeenen@gmail.com>
2022-04-02 21:39:45 -0400

    Fixed MyLibrary filter

--- frontend/src/pages/MyLibraryPage/MyLibraryPage.js
+++ frontend/src/pages/MyLibraryPage/MyLibraryPage.js
@@ -13,6 +13,11 @@ import woodshelf from "../../images/woodshelf.png"
 import UserContext from "../../user/UserContext"
 import SessionContext from "../../session/SessionContext"
 import BookInfoModal from "../../components/BookInfoModal/BookInfoModal"
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 
 const MyLibraryPage = () => {
@@ -26,7 +31,7 @@ const MyLibraryPage = () => {
     const [selectedBooks, setSelectedBooks] = useState([])
     const [readBooks, setReadBooks] = useState([])
     const [unreadBooks, setUnReadBooks] = useState([])
-    const [selectedGenre, setSelectedGenre] = useState("all")
+    const [selectedGenre, setSelectedGenre] = useState("")
     const [showRemoveBook, setShowRemoveBook] = useState(false);
     const [curBook, setCurBook] = useState(null);
 
@@ -90,9 +95,21 @@ const MyLibraryPage = () => {
         <div className="myLibrary">
             <h1 className="myLibraryTitle">My Library</h1>
             <div className="bookFilter">
-                <select value={selectedGenre} onChange={updateFilter}>
-                    {userGenres.map(genre => <option key={genre} value={genre}>{genre}</option>)}
-                </select>
+                <Box sx={{ minWidth: 120 }}>
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">Genre</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={selectedGenre}
+                            defaultValue="All"
+                            label="Genre"
+                            onChange={updateFilter}
+                        >
+                            {userGenres.map(genre => <MenuItem key={genre} value={genre}>{genre}</MenuItem>)}
+                        </Select>
+                    </FormControl>
+                </Box>
             </div>
 
             {selectedBooks.map((row, index) => {
